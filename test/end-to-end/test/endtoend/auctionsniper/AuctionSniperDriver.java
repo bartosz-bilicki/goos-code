ea08758a0c19c641de44447b064744b93c57672c
BB <bb.bartosz@gmail.com>
2013-08-29 22:26:20 +0200

    System.setProperty("com.objogate.wl.keyboard", "US") to make windowLicker work

--- test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,10 +1,9 @@
 package test.endtoend.auctionsniper;
 
-import static auctionsniper.ui.MainWindow.NEW_ITEM_ID_NAME;
-import static auctionsniper.ui.MainWindow.NEW_ITEM_STOP_PRICE_NAME;
-import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
-import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
-import static java.lang.String.valueOf;
+import static auctionsniper.ui.MainWindow.*;
+import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.*;
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.*;
+import static java.lang.String.*;
 
 import javax.swing.JButton;
 import javax.swing.JTextField;
@@ -21,44 +20,42 @@ import com.objogate.wl.swing.driver.JTextFieldDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 @SuppressWarnings("unchecked")
-public class AuctionSniperDriver extends JFrameDriver { 
-  public AuctionSniperDriver(int timeoutMillis) { 
-    super(new GesturePerformer(), 
-          JFrameDriver.topLevelFrame( 
-            named(MainWindow.MAIN_WINDOW_NAME), 
-            showingOnScreen()), 
-            new AWTEventQueueProber(timeoutMillis, 100)); 
-  }
-  
-  public void hasColumnTitles() { 
-    JTableHeaderDriver headers = new JTableHeaderDriver(this, 
-                                                        JTableHeader.class); 
-    headers.hasHeaders( 
-       matching(withLabelText("Item"), withLabelText("Last Price"), 
-                withLabelText("Last Bid"), withLabelText("State"))); 
-  } 
+public class AuctionSniperDriver extends JFrameDriver {
+	static {
+		System.setProperty("com.objogate.wl.keyboard", "US");
+	}
 
-  public void showsSniperStatus(String itemId, int lastPrice, int lastBid, String statusText) {
-    JTableDriver table = new JTableDriver(this); 
-    table.hasRow( 
-        matching(withLabelText(itemId), withLabelText(valueOf(lastPrice)), 
-               withLabelText(valueOf(lastBid)), withLabelText(statusText)));     
-  }
+	public AuctionSniperDriver(int timeoutMillis) {
+		super(new GesturePerformer(),
+				JFrameDriver.topLevelFrame(named(MainWindow.MAIN_WINDOW_NAME), showingOnScreen()),
+				new AWTEventQueueProber(timeoutMillis, 100));
+	}
 
-  public void startBiddingWithStopPrice(String itemId, int stopPrice) {
-    textField(NEW_ITEM_ID_NAME).replaceAllText(itemId); 
-    textField(NEW_ITEM_STOP_PRICE_NAME).replaceAllText(String.valueOf(stopPrice)); 
-    bidButton().click(); 
-  }
+	public void hasColumnTitles() {
+		JTableHeaderDriver headers = new JTableHeaderDriver(this, JTableHeader.class);
+		headers.hasHeaders(matching(withLabelText("Item"), withLabelText("Last Price"), withLabelText("Last Bid"),
+				withLabelText("State")));
+	}
 
-  private JTextFieldDriver textField(String fieldName) {
-    JTextFieldDriver newItemId = 
-      new JTextFieldDriver(this, JTextField.class, named(fieldName));
-    newItemId.focusWithMouse();
-    return newItemId;
-  }
+	public void showsSniperStatus(String itemId, int lastPrice, int lastBid, String statusText) {
+		JTableDriver table = new JTableDriver(this);
+		table.hasRow(matching(withLabelText(itemId), withLabelText(valueOf(lastPrice)),
+				withLabelText(valueOf(lastBid)), withLabelText(statusText)));
+	}
 
-  private JButtonDriver bidButton() {
-    return new JButtonDriver(this, JButton.class, named(MainWindow.JOIN_BUTTON_NAME));
-  } 
-} 
+	public void startBiddingWithStopPrice(String itemId, int stopPrice) {
+		textField(NEW_ITEM_ID_NAME).replaceAllText(itemId);
+		textField(NEW_ITEM_STOP_PRICE_NAME).replaceAllText(String.valueOf(stopPrice));
+		bidButton().click();
+	}
+
+	private JTextFieldDriver textField(String fieldName) {
+		JTextFieldDriver newItemId = new JTextFieldDriver(this, JTextField.class, named(fieldName));
+		newItemId.focusWithMouse();
+		return newItemId;
+	}
+
+	private JButtonDriver bidButton() {
+		return new JButtonDriver(this, JButton.class, named(MainWindow.JOIN_BUTTON_NAME));
+	}
+}
